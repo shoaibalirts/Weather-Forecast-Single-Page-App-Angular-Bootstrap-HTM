e10e92f3e2e01baef11939b379ac463a087ecd26
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-19 15:20:50 +0200

    Added controllers, routes, created two templates home and forecast. applied bootstrap classes to form elements

---
+++ App.js
@@ -0,0 +1,22 @@
+// MODULE
+var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
+
+// ROUTES
+weatherApp.config(function ($routeProvider) {
+  $routeProvider
+
+    .when("/home", {
+      templateUrl: "pages/home.html",
+      controller: "homeController",
+    })
+
+    .when("/forecast", {
+      templateUrl: "pages/forecast.html",
+      controller: "forecastController",
+    });
+});
+
+// CONTROLLERS
+weatherApp.controller("homeController", ["$scope", function ($scope) {}]);
+
+weatherApp.controller("forecastController", ["$scope", function ($scope) {}]);
